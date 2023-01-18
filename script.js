b29e47d7e448afed2d69fcd6657010aae89e572d
lucassaureliano <lucassaureliano@gmail.com>
2023-01-18 20:09:46 -0300

    Aula 8

---
+++ script.js
@@ -0,0 +1,12 @@
+const opcao = "Vermelho";
+
+switch(opcao){
+    case "Vermelho":
+        alert("Botão Vermelho selecionado");
+        break;
+    case "Azul":
+        alert("Botão Azul selecionado");
+        break;
+
+
+}
